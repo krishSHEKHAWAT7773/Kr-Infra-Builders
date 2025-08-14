7c12fdfd27a93327514f67a773f1e8fa9f7f1bb2
krishSHEKHAWAT7773 <krishanbnb03@gmail.com>
2025-08-14 13:03:27 +0530

    Add files via upload

--- dashboard.js
+++ dashboard.js
@@ -44,3 +44,10 @@ Object.keys(tabs).forEach(id => {
     });
   }
 });
+
+document.querySelectorAll(".nav-item").forEach(item => {
+  item.addEventListener("click", () => {
+    document.querySelectorAll(".nav-item").forEach(el => el.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
